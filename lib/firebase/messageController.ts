31fbaad67a5d057098655fb83d7a2c16f2c0d1c5
manish4329 <37350344+manish4329@users.noreply.github.com>
2023-10-13 16:10:26 +0530

    Type change of param in messageInput

--- lib/firebase/messageController.ts
+++ lib/firebase/messageController.ts
@@ -1,4 +1,10 @@
-import { arrayUnion, collection, doc, getFirestore, setDoc } from "firebase/firestore";
+import {
+  arrayUnion,
+  collection,
+  doc,
+  getFirestore,
+  setDoc,
+} from "firebase/firestore";
 import { auth, firestoreApp } from ".";
 import { getSingleUserFromFirestore } from "./userController";
 
@@ -6,26 +12,27 @@ export const firestore = getFirestore(firestoreApp);
 
 export const chatsCollection = collection(firestore, "chats");
 
-export const createNewChat = async (messageText : string, routerId: string) => {
-    const currentUser = auth?.currentUser;
-    const recipientDetails = await getSingleUserFromFirestore(routerId);
-    const chatRef = doc(firestore, "chats", routerId);
-    await setDoc(
-        chatRef, {
-            link: routerId,
-            messages: {
-                message: arrayUnion({
-                    messageBody: messageText,
-                    createdAt: new Date(),
-                    messageSender: currentUser?.email,
-                    messageSenderId: currentUser?.uid,
-                    // this is only used for the first index of the Array
-                    messageRecipient: recipientDetails?.email,
-                    messageRecipientId: recipientDetails?.uid,
-                }),
-
-            },
-        },
-        {merge: true}
-    );
+export const createNewChat = async (messageText: string, routerId: string) => {
+  const currentUser = auth?.currentUser;
+  const recipientDetails = await getSingleUserFromFirestore(routerId);
+  console.log(recipientDetails, "recipient");
+  const chatRef = doc(firestore, "chats", routerId);
+  await setDoc(
+    chatRef,
+    {
+      link: routerId,
+      messages: {
+        message: arrayUnion({
+          messageBody: messageText,
+          createdAt: new Date(),
+          messageSender: currentUser?.email,
+          messageSenderId: currentUser?.uid,
+          // this is only used for the first index of the Array
+          // messageRecipient: recipientDetails?.email,
+          // messageRecipientId: recipientDetails?.uid,
+        }),
+      },
+    },
+    { merge: true }
+  );
 };
--- lib/firebase/userController.ts
+++ lib/firebase/userController.ts
@@ -1,38 +1,47 @@
 import { User } from "firebase/auth";
-import { DocumentData, collection, doc, getDoc, getFirestore, serverTimestamp, setDoc } from "firebase/firestore";
+import {
+  DocumentData,
+  collection,
+  doc,
+  getDoc,
+  getFirestore,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
 import { firestoreApp } from ".";
 
 export const firestore = getFirestore(firestoreApp);
 
-export const usersCollection = collection(firestore, 'users');
+export const usersCollection = collection(firestore, "users");
 
 export const addUserToFirestore = async (user: User) => {
-    // if user already exists in firestore collection, update the last seen to now 
-    // if user exists add the user to the firestore, add the user to firestore.
-    const userRef = doc(firestore, "users", user.uid);
-    await setDoc(
-        userRef, {
-            uid: user.uid,
-            name: user.displayName,
-            email: user.email,
-            photo: user.photoURL,
-            lastOnline: serverTimestamp(),
-        },
-        { merge: true}
-    ); 
+  // if user already exists in firestore collection, update the last seen to now
+  // if user exists add the user to the firestore, add the user to firestore.
+  const userRef = doc(firestore, "users", user.uid);
+  await setDoc(
+    userRef,
+    {
+      uid: user.uid,
+      name: user.displayName,
+      email: user.email,
+      photo: user.photoURL,
+      lastOnline: serverTimestamp(),
+    },
+    { merge: true }
+  );
 };
 
 export function getSnapshotDoc(doc: DocumentData) {
-    return {
-        id: doc.id,
-        ...doc.data(),
-    };
-};
+  return {
+    id: doc.id,
+    ...doc.data(),
+  };
+}
 
 export const getSingleUserFromFirestore = async (userId: string) => {
-    if(!userId) return;
-    const userRef = doc(firestore,`users/${userId}`);
-    const userSnap = await getDoc(userRef);
-    const user = userSnap.data();
-    return user;
+  if (!userId) return;
+  const userRef = doc(firestore, `users/${userId}`);
+  const userSnap = await getDoc(userRef);
+  const user = userSnap.data();
+  return user;
 };
